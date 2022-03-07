bbb6fba1acf4b10d1b630dbdd4640b74c695124a
ZuGo06 <alichok_krasavchik@mail.ru>
2022-03-07 19:50:11 +0500

    project complete

---
+++ src/components/Movie.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Movie = (props) => {
+  const { Title, Year, imdbID: id, Type, Poster } = props
+
+  return (
+    <div id={id} className="card movie">
+      <div className="card-image waves-effect waves-block waves-light">
+        {Poster === 'N/A' ? (
+          <img
+            className="activator"
+            src={`https://via.placeholder.com/300x400?text=${Title}`}
+          />
+        ) : (
+          <img className="activator" src={Poster} />
+        )}
+      </div>
+      <div className="card-content">
+        <span className="card-title activator grey-text text-darken-4">
+          {Title}
+        </span>
+        <p>
+          {Year} <span className="right">{Type}</span>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Movie
